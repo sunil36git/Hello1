ec2c6d29e1b1f4f052869bd8daaf2ac557d3b09f
Sunil <sunil_kumar@test.com>
2018-12-23 23:40:29 -0600

    test

--- src/main/java/com/test/Hello1/web/AppController.java
+++ src/main/java/com/test/Hello1/web/AppController.java
@@ -24,7 +24,7 @@ public class AppController {
 	{
 		logger.info("Request received for name..." + name);
 		logger.info("Response sent for name..." + name);
-		return "Hello aaaaa.... "+name;
+		return "Hello bbb.... "+name;
 
 	}
 	
